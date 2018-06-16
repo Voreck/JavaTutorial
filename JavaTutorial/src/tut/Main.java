4bf0d8c9e5d35ff588a384b89726b467489dcfce
Voreck <lukvok12@gmail.com>
2018-06-16 19:54:00 +0200

    Erweiterte Dokumentation

--- JavaTutorial/src/tut/Main.java
+++ JavaTutorial/src/tut/Main.java
@@ -3,11 +3,15 @@ package tut;
 /**
  * Die Haupt-Klasse dieses Tutorials
  * @author Voreck
- * @version 1.0.0-2,   06.16.18
+ * @version 1.0.0-3,   06.16.18
  */
 public class Main {
 	//Jede ausführbare Code-Zeile muss in einer Klasse stehen
 	
+	/**
+	 * Einstiegspunkt des Programms
+	 * @param args Commandline argumente
+	 */
 	public static void main (String[]args) {
 		//Die main Methode ist der Einstiegspunkt jedes Programms
 		System.out.println("Hello World");
