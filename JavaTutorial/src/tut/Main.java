82182a5bb54a60731ef180337c223e01b205337e
Voreck <lukvok12@gmail.com>
2018-06-16 19:36:39 +0200

    Initial commit

---
+++ JavaTutorial/src/tut/Main.java
@@ -0,0 +1,15 @@
+package tut;
+
+/**
+ * Die Haupt-Klasse dieses Tutorials
+ * @author Voreck
+ * @version 1.0.0-2,   06.16.18
+ */
+public class Main {
+	//Jede ausführbare Code-Zeile muss in einer Klasse stehen
+	
+	public static void main (String[]args) {
+		//Die main Methode ist der Einstiegspunkt jedes Programms
+		System.out.println("Hello World");
+	}
+}
---
+++ JavaTutorial/src/tut/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Das Standard-package dieses Tutorials
+ * {@docRoot}
+ */
+package tut;
+/*Packages sind Ordner zur Strukturierung des Projekts.
+ * Es sollte immer mindestens ein package vorhanden sein.
+ */
